a5ea476d268710f9fffb6393f03e59c8ea1abfc0
jorge-73 <jorge_ap_73@hotmail.com>
2024-03-01 22:19:45 -0300

    updates

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -114,11 +114,11 @@ const initializePassport = () => {
             // Si el usuario ya existe en la base de datos, generamos el token
             const token = generateToken(existingUser);
             // Actualizar solo la propiedad last_connection
-            user.last_connection = new Date();
-            await UserService.update(user._id, {
-              last_connection: user.last_connection,
+            existingUser.last_connection = new Date();
+            await UserService.update(existingUser._id, {
+              last_connection: existingUser.last_connection,
             });
-            devLogger.info(user);
+            devLogger.info(existingUser);
             // Enviamos el token como una cookie en la respuesta
             return done(null, existingUser, { token });
           }
